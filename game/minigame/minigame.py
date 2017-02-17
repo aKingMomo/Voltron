dc4f853ee2368e4958401d533c67cd05d146c353
Ashiok <ash.ewok@gmail.com>
2017-02-16 22:02:46 -0500

    Galraga!!
    Now the game plays like a game. Speeds need tweaking still, and the UI
    needs a facelift and instructions. Current highscore: 16600

--- game/minigame/minigame.py
+++ game/minigame/minigame.py
@@ -2,113 +2,94 @@ import pygame
 import math
 import random
 
-#color definitions
-BLACK = (0,0,0)
-WHITE = (255,255,255)
-CYAN = (0,255,255)
 
-enemy_speed = 5
+# color definitions
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+CYAN = (0, 255, 255)
 
-
-
-
-#character is name of player sprite, either Castle, Red, Green, Blue, Yellow or Black
-#TODO when adding to renpy: Select character based on points
-character = "Castle"
-
+global enemy_speed
+enemy_speed = 3.0
+# character is name of player sprite file
+# options:Castle, Red, Green, Blue, Yellow or Black
+# TODO when adding to renpy: Select character based on points
+character = "Black"
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self,xtarget,ytarget):
-            pygame.sprite.Sprite.__init__(self)
-            self.image = pygame.image.load("Galra.png").convert()
-            self.image.set_colorkey(BLACK)
-            self.rect = self.image.get_rect()
-            self.xpos = 0.0
-            self.ypos = 0.0
-            self.xspeed = enemy_speed
-            self.yspeed = 0
-            #new movement code - takes the coordinates of the player sprite, and sets enemy initial velocity towards the player
-            #for this, xpos (initial x) is set to a random float between 0.0 and 1000.0 and ypos (initial y) is 0.0
-            #self.rect.x and y are set to x and y pos in the init loop
-            """if abs(xtarget - self.xpos) >=50:
-            	self.yspeed = -enemy_speed * (self.ypos - ytarget)/abs(self.xpos - xtarget)
-            	self.xspeed = -enemy_speed * (self.xpos - xtarget)/abs(self.ypos - ytarget)
+    def __init__(self, xtarget, ytarget):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("Galra.png").convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.x = float(random.randrange(0, 1280))
+        self.rect.y = 0.0 - self.rect.height
+        if xtarget - self.rect.x != 0:
+            self.theta = math.atan(
+                abs((ytarget - self.rect.y) / (xtarget - self.rect.x)))
+            if xtarget - self.rect.x > 0:
+                self.xspeed = math.cos(self.theta) * enemy_speed
             else:
-            	self.yspeed = enemy_speed
-            	self.xspeed = 0"""
-            self.health = 1
+                self.xspeed = - math.cos(self.theta) * enemy_speed
 
-
-    #updates position of enemy, returns True if past edge of screen
-    def update(self):
-       #check if past boundary (will be destroyed)
-        
-        if self.rect.x >= 1300 or self.rect.y>=800:
-            return True
-        
-        #old movement code
+            self.yspeed = math.sin(self.theta) * enemy_speed
         else:
-            
-            if self.rect.x == 1150: 
-                self.xspeed = 0
-                self.yspeed = enemy_speed
-            if self.rect.y == 160:
-                self.xspeed = -enemy_speed
-                self.yspeed = 0
-            if self.rect.x == 10:
-                self.xspeed = 0
-                self.yspeed = enemy_speed
-            if self.rect.y == 300:
-                self.xspeed = enemy_speed
-                self.yspeed = 0
+            self.yspeed = enemy_speed
+            self.xspeed = 0
 
+        self.health = 1
+
+    # updates position of enemy, returns True if past edge of screen
+    def update(self):
+        # check if past boundary (will be destroyed)
+        if self.rect.x >= 1300 or self.rect.x <= -100 or self.rect.y >= 800:
+            return True
+        else:
             self.rect.x += self.xspeed
             self.rect.y += self.yspeed
             return False
 
-    #called when hit by projectile, returns true if to be destroyed
+    # called when hit by projectile, returns true if to be destroyed
     def collide(self):
-            self.health -= 1
-            if self.health == 0:
-                return True
-            else:
-                return False
-
+        self.health -= 1
+        if self.health == 0:
+            return True
+        else:
+            return False
 
 
 class Projectile(pygame.sprite.Sprite):
     def __init__(self):
-            pygame.sprite.Sprite.__init__(self)
-            self.image = pygame.Surface([10,30])
-            self.image.fill(CYAN)
-            self.rect = self.image.get_rect()
-            self.yspeed = -10
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([10, 30])
+        self.image.fill(CYAN)
+        self.rect = self.image.get_rect()
+        self.yspeed = -10
 
-    #update the projectile position, returns true if it has passed the top of the screen
+    # update the projectile position
+    # returns true if it has passed the top of the screen
     def update(self):
-            if self.rect.y <= -self.rect.y:
-                return True
-            else:
-                self.rect.y += self.yspeed
-                return False
+        if self.rect.y <= -self.rect.y:
+            return True
+        else:
+            self.rect.y += self.yspeed
+            return False
 
-    
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-			pygame.sprite.Sprite.__init__(self)
-			self.image = pygame.image.load("%s.png" %(character)).convert()
-			self.image.set_colorkey(BLACK)
-			self.rect = self.image.get_rect()
-			self.health =3
-		
-			
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("%s.png" % (character)).convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.health = 5
 
-        
+
 def main():
     pygame.init()
 
-    size = (1280,720)
+    global enemy_speed
+    size = (1280, 720)
     screen = pygame.display.set_mode(size)
 
     font = pygame.font.SysFont('Calibri', 25, True, False)
@@ -117,62 +98,43 @@ def main():
     x_coord = 50
     y_coord = 600
 
-    #list of enemy sprites
+    # list of enemy sprites
     enemy_list = pygame.sprite.Group()
-    
-    #list of all sprites
+    # list of all sprites
     all_sprites_list = pygame.sprite.Group()
-    
-
-    #list of shots
+    # list of shots
     projectile_list = pygame.sprite.Group()
 
     # create player
     player = Player()
     all_sprites_list.add(player)
-    
-    # boolean that ends game loop
+    # booleans that alter game loop
     done = False
-    go = True
-    reset= False
-    #manage screen update
+    pause = False
+    reset = False
+    # manage screen update
     clock = pygame.time.Clock()
-
-    
-
-    #how fast the player moves
-    player_speed = 10
-    xspeed=0
-    yspeed=0
- 
-    # Enemy spawn position
-    e_x_start = 20
-    e_y_start = 20
-
-
-
-    
-
-
-    #score
+    # how fast the player moves
+    player_speed = 5
+    xspeed = 0
+    yspeed = 0
+    # Enemy information - spawn timer and movespeed
+    enemy_spawn = 50
+    enemy_speed = 3.0
+    # score
     score = 0
-    
-
+    # housekeeping variables (count is time, state is for game progression)
     count = 0
-
-    #should we shoot a projectile?
-    shoot=False
-
-
-    #number of enemies
+    state = 0
+    # should we shoot a projectile?
+    shoot = False
 
     # -----Main Loop -----
     while not done:
-        
         # --- Main event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                done = True #this ends the loop
+                done = True  # this ends the loop
             # User pressed down on a key
             elif event.type == pygame.KEYDOWN:
                 # Figure out if it was an arrow key. If so
@@ -186,12 +148,16 @@ def main():
                 elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     yspeed = player_speed
 
-				#create projectile if spacebar is held down and there are no other projectiles
+                # Create projectile if spacebar is held down
+                # and you are allowed to have more by your score
                 elif event.key == pygame.K_SPACE:
-       				shoot = projectile_list.__len__() == 0
+                    shoot = projectile_list.__len__() <= state / 2
+                # Reset the game if the player is dead and presses R
                 elif event.key == pygame.K_r:
-                	reset = player.health<=0
-
+                    reset = player.health <= 0
+                # Pause/unpause the game if the player presses P
+                elif event.key == pygame.K_p:
+                    pause = not pause
 
             # User let up on a key
             elif event.type == pygame.KEYUP:
@@ -201,111 +167,118 @@ def main():
                 elif event.key == pygame.K_UP or event.key == pygame.K_DOWN or event.key == pygame.K_w or event.key == pygame.K_s:
                     yspeed = 0
 
-        
-
-
-        
-        
-        #player movement
-        
         # --Game logic goes here
-
-        #if the player is alive
-        if player.health>0:
-
-        	#movement
-        	if x_coord + xspeed > 0 and x_coord + xspeed < 1175:
-        		x_coord+=xspeed
-			if y_coord + yspeed > 0 and y_coord + yspeed < 625: 
-				y_coord+=yspeed
-			player.rect.x=x_coord
-			player.rect.y=y_coord
-
-			#create a new player projectile if we need to
-	        if shoot:
-	        	projectile = Projectile()
-	     		projectile.rect.x = x_coord +player.rect.width/2
-	       		projectile.rect.y = y_coord
-	        	projectile_list.add(projectile)
-	        	all_sprites_list.add(projectile)
-	        	shoot = False
-
-
-	       #update projectile
-	        for p in projectile_list:
-	            if Projectile.update(p):
-	                projectile_list.remove(p)
-	                all_sprites_list.remove(p) 
-
-
-	        #update enemy
-	        for e in enemy_list:
-	        	#if it's offscreen, delete it
-	            if Enemy.update(e):
-	                enemy_list.remove(e)
-	                all_sprites_list.remove(e)
-	                player.health -=1
-
-
-	            
-	            #check if it's been hit by projectile
-	            else:
-	                projectiles_hit_list = pygame.sprite.spritecollide(e, projectile_list, True)
-	                for p in projectiles_hit_list:
-	                    if Enemy.collide(e):
-	                        score += 100
-	                        enemy_list.remove(e)
-	                        all_sprites_list.remove(e)
-
-	        #check if player hit by enemy (only relevant for the other type of motion or if things change)
-	        enemy_hit_list = pygame.sprite.spritecollide(player, enemy_list, True)
-	        for e in enemy_hit_list:
-	        	player.health-=1
-
-
-    	    #make new enemy       
-
-	        count += 1
-	        if(len(enemy_list)<50 and count%50==0):
-
-	            enemy = Enemy(x_coord,y_coord)
-	            enemy.rect.x = e_x_start
-	            enemy.rect.y = e_y_start
-	            enemy_list.add(enemy)
-	            all_sprites_list.add(enemy)
-
-
-	        #update score (and count for test purposes)
-	        text = font.render("Current score: " + str(score) +" Count: " + str(count) + " Health: " +str(player.health),True, WHITE)
-        
+        if pause:
+            # TODO: Pause menu
+            text = font.render(
+                "The game is paused. Please press P to continue", True, WHITE)
         else:
-	        text = font.render("Game over! You scored " + str(score) +" points. Press R to play again!",True, WHITE)
-	        if reset:
-	        	enemy_list.empty()
-	        	projectile_list.empty()
-	        	all_sprites_list.empty()
-	        	player = Player()
-    			all_sprites_list.add(player)
-    			reset = False
-    			score = 0
-    			count = 0
+            # If the player is dead, the gameover procedure
+            if player.health < 0:
+                text = font.render("Game over! You scored " + str(score) +
+                                   " points. Press R to play again!", True, WHITE)
+                # Return changed parameters to initial values to restart
+                # TODO: countdown before restart?
+                if reset:
+                    enemy_list.empty()
+                    projectile_list.empty()
+                    all_sprites_list.empty()
+                    player = Player()
+                    all_sprites_list.add(player)
+                    reset = False
+                    score = 0
+                    count = 0
+                    xspeed = 0
+                    yspeed = 0
+                    x_coord = 50
+                    y_coord = 600
+                    state = 0
 
+            # If the game is running and the player is alive
+            else:
+                # Player Movement
+                if x_coord + xspeed > 0 and x_coord + xspeed < 1175:
+                    x_coord += xspeed
+                if y_coord + yspeed > 0 and y_coord + yspeed < 625:
+                    y_coord += yspeed
+                player.rect.x = x_coord
+                player.rect.y = y_coord
 
+                # Create a new player projectile if we need to
+                if shoot:
+                    projectile = Projectile()
+                    projectile.rect.x = x_coord + player.rect.width / 2
+                    projectile.rect.y = y_coord
+                    projectile_list.add(projectile)
+                    all_sprites_list.add(projectile)
+                    shoot = False
 
+                # Update projectile
+                for p in projectile_list:
+                    if Projectile.update(p):
+                        projectile_list.remove(p)
+                        all_sprites_list.remove(p)
 
-	            
-	        # --Drawing goes here        
-    	screen.blit(background_image, [0, 0])
-    	all_sprites_list.draw(screen)
+                # Update enemy
+                for e in enemy_list:
+                    # If it's offscreen, delete it (maybe penalize player?)
+                    if Enemy.update(e):
+                        enemy_list.remove(e)
+                        all_sprites_list.remove(e)
 
-    	screen.blit(text, (600, 600))
+                    # Check if it's been hit by projectile
+                    else:
+                        projectiles_hit_list = pygame.sprite.spritecollide(
+                            e, projectile_list, True)
+                        for p in projectiles_hit_list:
+                            if Enemy.collide(e):
+                                score += 100
+                                enemy_list.remove(e)
+                                all_sprites_list.remove(e)
 
-    	pygame.display.flip()
+                # Check if player hit by enemy
+                enemy_hit_list = pygame.sprite.spritecollide(
+                    player, enemy_list, True)
+                for e in enemy_hit_list:
+                    player.health -= 1
 
-    	clock.tick(40)
+                # Make new enemy, up to a max of ten
+                # This spawns with a random X coordinate, above the screen
+                # It moves towards the Player
+                count += 1
+                if(len(enemy_list) < 10 and count % enemy_spawn == 0):
+                        # Calculate current speeds and state
+                        # every 1K points, the enemies spawn sooner (max 6 per sec)
+                        # every 2K, the enemies move faster
+                        # every 2K you can shoot another projectile
+                    state = score / 1000
+                    enemy_spawn = 50 - 5 * state
+                    if state >= 9:
+                        enemy_spawn = 10
+                    enemy_speed = 3.0 + 1.5 * (state / 2)
 
+                    enemy = Enemy(x_coord, y_coord)
+                    enemy_list.add(enemy)
+                    all_sprites_list.add(enemy)
+
+                # Update score (and test variables for test purposes)
+                text = font.render("Current score: " + str(score) +
+                                   " Count: " + str(count) +
+                                   " Health: " + str(player.health) +
+                                   " Enemy count: " + str(len(enemy_list)), True, WHITE)
+
+        # --Drawing goes here
+        screen.blit(background_image, [0, 0])
+        all_sprites_list.draw(screen)
+
+        screen.blit(text, (300, 600))
+
+        pygame.display.flip()
+
+        clock.tick(60)
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
